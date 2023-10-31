06cfd5e35860b120997a3fa64a76e98378af9dd7
Ibtissamibtissam <ibtissambouaid20@gmail.com>
2023-10-31 17:35:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './Style.css'
+import econ from './Images/left-arrow.svg'
+import econ2 from './Images/right-arrow.svg'
+import image1 from './Images/image 1.jpg'
+import image2 from './Images/image2.jpg'
+import image3 from './Images/image3.jpg'
+import image4 from './Images/image 4.jpg'
 
-function App() {
+
+
+
+
+const sliderData = [
+  {
+    id: 1,
+    image: image1,
+    caption: 'Image 1',
+  },
+  {
+    id: 2,
+    image: image2,
+    caption: 'Image 2',
+  },
+  {
+    id: 2,
+    image: image3,
+    caption: 'Image 3',
+  },
+  {
+    id: 1,
+    image: image4,
+    caption: 'Image 4',
+  }
+  // Ajoutez d'autres URL d'images si nécessaire
+];
+
+const Slider = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % sliderData.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentImageIndex(
+      (prevIndex) => (prevIndex - 1 + sliderData.length) % sliderData.length
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <center>
+    <div className="slider" id='a'>
+      <div id='b' style={{width :'25px', height:'20px'}} onClick={prevSlide}><img src={econ}></img></div>
+      <img id='img' style={{width :'800px', height:'500px'}} src={sliderData[currentImageIndex].image} alt={sliderData[currentImageIndex].caption} />
+      <div id='c' style={{width :'25px', height:'20px'}} onClick={nextSlide}><img src={econ2}></img></div>
     </div>
+    </center>
   );
-}
+};
 
-export default App;
+export default Slider;
